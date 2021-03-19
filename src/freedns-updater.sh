b8b95503cc0f3554deadabb89294efd06cf5806e
José Correia <javcorreia@hotmail.com>
2021-03-19 10:44:26 +0000

    ip update fix

--- src/freedns-updater.sh
+++ src/freedns-updater.sh
@@ -4,11 +4,13 @@ LAST_IP=`cat ./freedns_last_ip`
 
 TOTAL_KEYS=`jq -r '.total' ./config.json`
 for (( c=0; c<$TOTAL_KEYS; c++ ))
-do  
+do
     KEY=`jq -r '.keys['$c']' ./config.json`
     if [ "$CURRENT_IP" != "$LAST_IP" ]; then
         echo $CURRENT_IP > ./freedns_last_ip
-        
+
         wget --no-check-certificate -O - https://freedns.afraid.org/dynamic/update.php?$KEY >> ./freedns_updater.log
     fi
 done
+
+echo $CURRENT_IP > ./freedns_last_ip
